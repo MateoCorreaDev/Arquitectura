66d339f01d852c493edf47b6f101ebee7e0d39cd
DoseiBrayin <ardilabrayan68@gmail.com>
2024-04-03 00:09:58 -0500

    (Style) create styles with tailwind

--- src/UserCrud/components/cardUser.jsx
+++ src/UserCrud/components/cardUser.jsx
@@ -2,10 +2,10 @@ import PropTypes from 'prop-types';
 
 export function CardUser({name, description, price}) {
     return (
-        <div>
-            <h2>{name}</h2>
-            <p>{description}</p>
-            <span>{price}</span>
+        <div className='shadow-lg bg-white p-4 rounded'>
+            <h2 className='text-xl font-bold'>{name}</h2>
+            <p className='text-gray-600'>{description}</p>
+            <span className='text-gray-500'>{price}</span>
         </div>
     )
 }
--- src/UserCrud/pages/UserMain.jsx
+++ src/UserCrud/pages/UserMain.jsx
@@ -1,25 +1,26 @@
-import {CardUser} from '../components/cardUser'
-import {useUser} from '../hooks/userHook'
+import { CardUser } from '../components/cardUser'
+import { useUser } from '../hooks/userHook'
 
 export function UserMain() {
     const user = useUser()
     return (
-        <div>
-            <h1 className='text-3xl font-bold underline'>User Main</h1>
-            {
-                user.map((user) => {
-                    return (
-                        <div key={user.name}>
-                            <CardUser
-                                name={user.name}
-                                description={user.description}
-                                price={user.price}
-                            />
-                        </div>
-                    )
-                })
-            }
-
-        </div>
+        <>
+            <h1 className='text-3xl font-bold underline p-4'>User Main</h1>
+            <section className='grid grid-cols-2 gap-4 p-6'>
+                {
+                    user.map((user) => {
+                        return (
+                            <div key={user.name} >
+                                <CardUser
+                                    name={user.name}
+                                    description={user.description}
+                                    price={user.price}
+                                />
+                            </div>
+                        )
+                    })
+                }
+            </section>
+        </>
     )
 }
